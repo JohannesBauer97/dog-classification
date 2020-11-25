f8a14371635a1966da37b4d7f3885f9f39cb45ab
Johannes Bauer <w4terfront@gmail.com>
2020-11-25 21:51:30 +0100

    modeljs without data augmentation

--- tensorflow/model-convert-to-js.py
+++ tensorflow/model-convert-to-js.py
@@ -2,4 +2,9 @@ import tensorflow as tf
 import tensorflowjs as tfjs
 
 model = tf.keras.models.load_model("model/1605955873")
+model = model.layers.pop(1)
+model.summary()
+
+# model.save("model/prod")
+
 tfjs.converters.save_keras_model(model, "modeljs/1605955873")
