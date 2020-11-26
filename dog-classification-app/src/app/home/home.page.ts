15a7c455df97d13aefc22a2356f78fe331e7dc80
Joba <w4terfront@gmail.com>
2020-11-26 17:51:55 +0100

    add model predict test

--- dog-classification-app/src/app/home/home.page.ts
+++ dog-classification-app/src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+const { Camera } = Plugins;
+import * as tf from '@tensorflow/tfjs';
+import { GraphModel } from '@tensorflow/tfjs';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('predictImg') predictImg: ElementRef;
+  img: string;
+  model: GraphModel;
 
-  constructor() {}
+  constructor() {
+    this.loadModel();
+  }
 
+  async takePhoto(): Promise<void>{
+    /*
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+     */
+    console.log(this.model.inputNodes);
+    const imageUrl = '/assets/test.jpg';
+    console.log(this.predictImg.nativeElement);
+    const tensors = tf.browser.fromPixels(this.predictImg.nativeElement);
+    console.log('tensors', tensors);
+    console.log('tensor dtype fix', tensors.toFloat());
+    const prediction = this.model.predict(tensors.toFloat().expandDims(0));
+    console.log('prediction', prediction);
+    // @ts-ignore
+    const score = tf.softmax(prediction);
+    console.log('score', score);
+  }
+
+  async loadModel(){
+    this.model = await tf.loadGraphModel('/assets/modeljs/model.json');
+  }
 }
