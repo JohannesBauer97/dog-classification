d2f316e9a581d94e44060e86f899a587ba911ccb
Joba <w4terfront@gmail.com>
2020-12-26 12:43:23 +0100

    ng update

--- dog-classification-app/src/app/home/home.page.spec.ts
+++ dog-classification-app/src/app/home/home.page.spec.ts
@@ -1,15 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {IonicModule} from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import {HomePage} from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [HomePage],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
